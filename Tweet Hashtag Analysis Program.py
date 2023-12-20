b3b7b4022af4b9f827d6db926fe0cdb4a64f229f
Monodeep Chahal <monodeepchahal7@gmail.com>
2023-12-19 21:10:13 -0500

    Third commit. The code that I added enhances its functionality by counting the frequency of each hashtag and visualizing this data. By integrating these steps, the program now not only extracts and organizes hashtag data but also provides insights into the most popular hashtags in your dataset through a visual representation.

--- Tweet Hashtag Analysis Program.py
+++ Tweet Hashtag Analysis Program.py
@@ -25,3 +25,20 @@ for tweet in tweets:
 # I created a DataFrame to store and manipulate the extracted hashtags
 # Each hashtag becomes a row in the DataFrame
 df = pd.DataFrame(hashtags, columns=['Hashtag'])
+
+# Data Aggregation: Count the frequency of each hashtag
+# Pandas value_counts() function provides a simple way to get the count of each hashtag
+hashtag_counts = df['Hashtag'].value_counts()
+
+# NumPy: Perform numerical operations (if needed)
+# This part can be used for additional numerical analysis like normalization
+
+# Matplotlib: Visualize the data
+# We create a bar plot to display the frequency of the top 10 hashtags
+top_hashtags = hashtag_counts.head(10)
+plt.figure(figsize=(10, 6))
+top_hashtags.plot(kind='bar')
+plt.title('Top 10 Hashtags in Tweets')
+plt.xlabel('Hashtag')
+plt.ylabel('Frequency')
+plt.show()
